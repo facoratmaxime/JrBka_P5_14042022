110030a8ed54fb21912968cae568cda42df8b96a
Maxime FACORAT <facorat.maxime@gmail.com>
2022-04-15 21:22:40 +0200

    Change regex to most commonly used for Email

--- front/js/cart.js
+++ front/js/cart.js
@@ -247,7 +247,7 @@ const regexAddress = (e) => {
 };
 
 //le champs doit contenir une suite de lettre, de chiffre, de "-" ou "_", suivi d'un @ suivi d"une suite de lettre et de chiffre de minimum 2 caractère, suivi d"un ".", suivi d"une suite de lettre compris entre 2 et 5 caractère
-var regMail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/
+var regMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 const regexEmail = (email) => {
   //input email
   //message d'erreur
